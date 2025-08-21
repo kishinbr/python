2ab2c796d310ab99a1c2527bfbf5c475be2949a8
kishinbr <viniciuskishibr@gmail.com>
2025-08-21 17:39:32 -0300

    finalizando lista encadeada

--- EstruturaDeDados/stack.py
+++ EstruturaDeDados/stack.py
@@ -15,8 +15,6 @@ class Stack:
 
         self.size +=1
 
-    
-
     def pop(self):
         if self.top is None:
             raise ValueError("Stack is empty")
@@ -25,13 +23,35 @@ class Stack:
         self.top = self.top.next
         self.size -= 1
         return data
+    
+    def peek(self):
+        if self.top is Node:
+            raise ValueError("stack is empty")
+        return self.top.data
 
+    def display(self):
+        if self.top is None:
+            raise ValueError("stack is empty")
+        
+        l = list()
+        current_node = self.top
+        while current_node:
+            l.append(current_node.data)
+            current_node = current_node.next
+        return l
+
+    def __len__(self):
+        return self.size
+    
 s = Stack()
 s.push(5)
 s.push(7)
 s.push(11)
+print(s.display())
 print(s.top.data)
 print(s.top.next.data)
 s.pop()
 print(s.top.data)
+
+
 
--- EstruturaDeDados/teste.py
+++ EstruturaDeDados/teste.py
@@ -42,6 +42,46 @@ class LinkedList:
             i+=1
             current_node = current_node.next
         raise ValueError(f"{elem} is not in list")
+    
+    def pop(self,index=None):
+        if self.head is None:
+            raise IndexError("pop from empty list")
+        if index is None:
+            index = self.size - 1
+        if index < 0 or index > self.size:
+            raise IndexError("list index out of range")
+        
+        if index == 0:
+            value = self.head.data
+            self.head = self.head.next
+        else:
+            current_node = self.head
+            for _ in range(index-1):
+                current_node = current_node.next
+            value = current_node.next.data
+            current_node.next = current_node.next.next
+
+        self.size -=1
+        return value
+    
+    def remove(self,elem):
+        current_node = self.head
+        previus_node = None
+        while current_node:
+            if current_node.data == elem:
+                if previus_node:
+                    previus_node.next =current_node.next
+                else:
+                    self.head = current_node.next
+                self.size -=1
+                return
+            previus_node = current_node
+            current_node = current_node.next
+
+        raise ValueError(f"{elem} is not in list")
+
+
+        
 
     def __contains__(self,elem):
         current_node = self.head
@@ -72,13 +112,26 @@ class LinkedList:
             current_node = current_node.next
             i+=1
         return i
-        
+    
+    def __str__(self):
+        elements = []
+        current_node = self.head
+        while current_node:
+            elements.append(str(current_node.data))
+            current_node = current_node.next
+        return " -> ".join(elements)
+    
+    def __iter__(self):
+        current_node = self.head
+        while current_node:
+            yield current_node.data
+            current_node = current_node.next
 
 
 l = LinkedList()
 l.append(5)
 l.append(7)
 l.append(8)
-print(l.head.next.data)
-a = l.getitem(2)
-print(a)
+ll = list(l)
+print(l)
+print(ll)
