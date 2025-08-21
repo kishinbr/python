e7fc1bbbb5adebfd128deee6b431e86db92ac287
kishinbr <viniciuskishibr@gmail.com>
2025-08-21 18:17:59 -0300

    append

--- EstruturaDeDados/doublylinkedlist.py
+++ EstruturaDeDados/doublylinkedlist.py
@@ -3,4 +3,20 @@ class Node:
         self.data = data
         self.next = None
         self.prev = None
-        
+
+class DoublyLinkedList:
+    def __init__(self):
+        self.head = None
+        self.tail = None
+        self.size = 0
+
+    def append(self,elem):
+        new_node = Node(elem)
+
+        if self.head is None:
+            self.head = new_node
+            self.tail = new_node
+        else:
+            self.tail.next = new_node
+            new_node.prev = self.tail
+            self.tail = new_node
